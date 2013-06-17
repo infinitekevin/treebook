dc7ca16041cac17c76c343a2f8de970891ed8080
Kevin Lehtiniitty <kevin@tinitt.com>
2013-06-17 17:09:24 -0400

    changes to user management

--- db/migrate/20130617204701_devise_create_users.rb
+++ db/migrate/20130617204701_devise_create_users.rb
@@ -1,6 +1,10 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      ## Profile information
+      t.string :first_name
+      t.string :last_name
+      t.string :profile_name
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
