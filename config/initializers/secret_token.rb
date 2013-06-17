8b1ab989282e170143b02c35aa2a04ee3946e9dd
Kevin Lehtiniitty <kevin@tinitt.com>
2013-06-17 14:09:15 -0400

    Treebook
    Treebook social network from Treehouse

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'fe99d27bfc22a104eae496a1c741d373f2fca3ca8b9b83c5cee19082a39d4f9b413cd14c34bfa7536628042bef568ce0ca2a48d8cacec556ca5df1f1fe084115'
