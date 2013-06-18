dd8c1863204082dcbe4225f9668940ff71795fe7
Kevin Lehtiniitty <kevin@tinitt.com>
2013-06-18 14:37:39 -0400

    added user signup validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,26 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
-  has_many :statuses
+  validates :first_name, presence: true;
 
-  def full_name
-  	first_name + " " + last_name
+  validates :last_name, presence: true;
+
+  validates :profile_name, presence: true, 
+  						   	uniqueness: true, 
+  						   	format: {
+  						   		with: /a-zA-Z0-9/,
+  								message: 'Must be formatted correctly.'
+  							}
+
+  	has_many :statuses
+
+  	def full_name
+  		first_name + " " + last_name
+  	end
   end
-end
