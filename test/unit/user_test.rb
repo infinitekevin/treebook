52232462e16af6af7e51cc1d7f98bbfdcbff9567
Kevin Lehtiniitty <kevin@tinitt.com>
2013-06-18 17:45:19 -0400

    fixed profile name validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -32,11 +32,21 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-  	user = User.new
+  	user = User.new(first_name: 'Kevin', last_name: 'Lehtiniitty', email: 'kevin2@tinitt.com')
+    user.password = user.password_confirmation = 'asdfasf'
+
   	user.profile_name = "My Profile With Spaces"
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Kevin', last_name: 'Lehtiniitty', email: 'kevin1234@tinitt.com')
+    user.password = user.password_confirmation = 'asdfasf123'
+
+    user.profile_name = "kevin_l_2"
+    assert user.valid?
   end
 end
